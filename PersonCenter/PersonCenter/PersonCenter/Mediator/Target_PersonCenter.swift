4cd68f35da2a37c61eefd96222007420750e31d2
fremaksim <lxwgh2007@gmail.com>
2018-10-31 13:53:16 +0800

    修改Taget_PersonCenter.swift func 参数为无Argument Label后为（_ pargms: ),修复在CTMediator class init = nil 的bug

--- PersonCenter/PersonCenter/PersonCenter/Mediator/Target_PersonCenter.swift
+++ PersonCenter/PersonCenter/PersonCenter/Mediator/Target_PersonCenter.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 @objc class Target_PersonCenter: NSObject {
-    @objc func Action_PersonCenterViewController(pargms: [String: Any]) -> UIViewController {
+    
+    @objc func Action_PersonCenterViewController(_ pargms: [String: Any]) -> UIViewController {
         if let callback = pargms["callback"] as? (String) -> Void {
         callback("Person Center init success")
         }
         let viewController = PersonCenterViewController()
         return viewController
     }
+    
 }
